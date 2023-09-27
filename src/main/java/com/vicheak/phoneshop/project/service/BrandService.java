808efe8f4da4d965a5553cdeec0bac789db0c064
Vicheak <suonvicheak991@gmail.com>
2023-09-28 00:55:32 +0700

    CRUD operation for Brand Model

--- src/main/java/com/vicheak/phoneshop/project/service/BrandService.java
+++ src/main/java/com/vicheak/phoneshop/project/service/BrandService.java
@@ -4,4 +4,6 @@ import com.vicheak.phoneshop.project.entity.Brand;
 
 public interface BrandService {
 	Brand create(Brand brand);
+	Brand getById(Integer id); 
+	Brand update(Integer id, Brand brandUpdate); 
 }
--- src/main/java/com/vicheak/phoneshop/project/service/impl/BrandServiceImpl.java
+++ src/main/java/com/vicheak/phoneshop/project/service/impl/BrandServiceImpl.java
@@ -1,9 +1,15 @@
 package com.vicheak.phoneshop.project.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.vicheak.phoneshop.project.entity.Brand;
+import com.vicheak.phoneshop.project.exception.ApiException;
+import com.vicheak.phoneshop.project.exception.ResourceNotFoundException;
 import com.vicheak.phoneshop.project.repository.BrandRepository;
 import com.vicheak.phoneshop.project.service.BrandService;
 
@@ -14,6 +20,36 @@ public class BrandServiceImpl implements BrandService{
 	
 	@Override
 	public Brand create(Brand brand) {
+		return brandRepository.save(brand);
+	}
+	
+	@Override
+	public Brand getById(Integer id) {
+		/*Optional<Brand> brandOptional = brandRepository.findById(id);
+		if(brandOptional.isPresent()) {
+			return brandOptional.get();
+		}*/
+		//throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Brand with id = " + id + " not found");
+		//throw new HttpClientErrorException(HttpStatus.NOT_FOUND, String.format("Brand with id = %d not found", id));
+		
+		/*return brandRepository.findById(id)
+				.orElseThrow(() -> 
+					new HttpClientErrorException(HttpStatus.NOT_FOUND, 
+							String.format("Brand with id = %d not found", id)));*/
+		
+		/*return brandRepository.findById(id)
+				.orElseThrow(() -> 
+					new ApiException(HttpStatus.NOT_FOUND, 
+							String.format("Brand with id = %d not found", id)));*/
+		
+		return brandRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Brand", id));
+	}
+	
+	@Override
+	public Brand update(Integer id, Brand brandUpdate) {
+		Brand brand = getById(id);
+		brand.setName(brandUpdate.getName()); //@TODO improve update
 		return brandRepository.save(brand);
 	}
 	
