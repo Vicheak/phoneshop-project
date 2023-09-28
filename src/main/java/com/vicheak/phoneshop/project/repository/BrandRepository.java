d316eb6495ea253b6302e23c43d56e43b75e07bd
Vicheak <suonvicheak991@gmail.com>
2023-09-28 17:36:37 +0700

    add jpa spec and pagination

--- src/main/java/com/vicheak/phoneshop/project/repository/BrandRepository.java
+++ src/main/java/com/vicheak/phoneshop/project/repository/BrandRepository.java
@@ -3,11 +3,14 @@ package com.vicheak.phoneshop.project.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.vicheak.phoneshop.project.entity.Brand;
 
 @Repository
-public interface BrandRepository extends JpaRepository<Brand, Integer>{
+public interface BrandRepository extends JpaRepository<Brand, Integer>, JpaSpecificationExecutor<Brand>{
+	//using JPA query method (containing)
+	List<Brand> findByNameLike(String name);
 	List<Brand> findByNameContaining(String name);
 }
