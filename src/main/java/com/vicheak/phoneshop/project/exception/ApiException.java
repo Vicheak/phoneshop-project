1dd156f993b9b86051bf66f8c8cd9114078de4f3
Vicheak <suonvicheak991@gmail.com>
2023-12-27 18:50:56 +0700

    update project

--- src/main/java/com/vicheak/phoneshop/project/exception/ApiException.java
+++ src/main/java/com/vicheak/phoneshop/project/exception/ApiException.java
@@ -3,10 +3,8 @@ package com.vicheak.phoneshop.project.exception;
 import org.springframework.http.HttpStatus;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
 public class ApiException extends RuntimeException {
 	private final HttpStatus status; 
 	private final String message;
