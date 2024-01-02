dbeaf4b0ce115d0b481bc247997f5295a1feb90b
Vicheak <suonvicheak991@gmail.com>
2024-01-02 12:23:24 +0700

    add audit entity

--- src/main/java/com/vicheak/phoneshop/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/vicheak/phoneshop/project/service/impl/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.vicheak.phoneshop.project.service.impl;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import com.vicheak.phoneshop.project.config.security.AuthUser;
 import com.vicheak.phoneshop.project.config.security.RoleEnum;
 import com.vicheak.phoneshop.project.config.security.UserService;
+import com.vicheak.phoneshop.project.entity.Role;
 import com.vicheak.phoneshop.project.entity.User;
 import com.vicheak.phoneshop.project.exception.ApiException;
 import com.vicheak.phoneshop.project.repository.UserRepository;
@@ -33,7 +38,8 @@ public class UserServiceImpl implements UserService {
 		AuthUser authUser = AuthUser.builder()
 				.username(user.getUsername())
 				.password(user.getPassword())
-				.authorities(user.getRole().getAuthorities())
+				//.authorities(user.getRole().getAuthorities())
+				.authorities(getAuthorities(user.getRoles()))
 				.accountNonExpired(user.isAccountNonExpired())
 				.accountNonLocked(user.isAccountNonLocked())
 				.credentialsNonExpired(user.isCredentialsNonExpired())
@@ -41,6 +47,25 @@ public class UserServiceImpl implements UserService {
 				.build();
 		
 		return Optional.ofNullable(authUser);
+	}
+	
+	private Set<SimpleGrantedAuthority> getAuthorities(Set<Role> roles){
+		
+		Set<SimpleGrantedAuthority> authorities1 = roles.stream()
+			.map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+			.collect(Collectors.toSet()); 
+		
+		Set<SimpleGrantedAuthority> authorities = roles.stream()
+			.flatMap(this::toStream)
+			.collect(Collectors.toSet());
+		
+		authorities.addAll(authorities1);
+		return authorities;
+	}
+	
+	private Stream<SimpleGrantedAuthority> toStream(Role role){
+		return role.getPermissions().stream()
+				.map(permission -> new SimpleGrantedAuthority(permission.getName()));
 	}
 
 }
